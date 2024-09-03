0ff9dbe3a6dbc7c922033b23506a48e5fed2b11a
Ciaran McCormick <ciaran@ciaranmccormick.com>
2024-09-03 19:34:27 +0100

    UPGRADES: Update various packages
    - Upgrade and migrate code to pydantic 2
    - Upgrade gtfs proto buffer package to 1.0
    - Other upgrades to dev dependencies

--- bods_client/models/timetables.py
+++ bods_client/models/timetables.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 from typing import List, Optional
 
@@ -27,42 +28,39 @@ class Timetable(BaseDataset):
     extension: str
     lines: List[str]
     first_start_date: datetime = Field(alias="firstStartDate")
-    first_end_date: datetime = Field(None, alias="firstEndDate")
-    last_end_date: datetime = Field(None, alias="lastEndDate")
+    first_end_date: datetime = Field(default=None, alias="firstEndDate")
+    last_end_date: datetime = Field(default=None, alias="lastEndDate")
     admin_areas: List[AdminAreas] = Field(alias="adminAreas")
     localities: List[Locality]
     dq_score: str = Field(alias="dqScore")
     dq_rag: str = Field(alias="dqRag")
-    bods_compliance: bool = Field(None, alias="bodsCompliance")
+    bods_compliance: Optional[bool] = Field(default=None, alias="bodsCompliance")
 
 
 class TimetableParams(BaseAPIParams):
-    class Config(BaseAPIParams.Config):
-        pass
-
-    admin_areas: Optional[AdminAreasType] = Field(None, alias="adminArea")
+    admin_areas: Optional[AdminAreasType] = Field(default=None, alias="adminArea")
     search: Optional[str] = None
-    modified_date: Optional[datetime] = Field(None, alias="modifiedDate")
-    start_date_start: Optional[datetime] = Field(None, alias="startDateEnd")
-    start_date_end: Optional[datetime] = Field(None, alias="startDateStart")
-    end_date_start: Optional[datetime] = Field(None, alias="endDateStart")
-    end_date_end: Optional[datetime] = Field(None, alias="endDateEnd")
-    dq_rag: Optional[str] = Field(None, alias="dqRag")
-    bods_compliance: Optional[bool] = Field(None, alias="bodsCompliance")
+    modified_date: Optional[datetime] = Field(default=None, alias="modifiedDate")
+    start_date_start: Optional[datetime] = Field(default=None, alias="startDateEnd")
+    start_date_end: Optional[datetime] = Field(default=None, alias="startDateStart")
+    end_date_start: Optional[datetime] = Field(default=None, alias="endDateStart")
+    end_date_end: Optional[datetime] = Field(default=None, alias="endDateEnd")
+    dq_rag: Optional[str] = Field(default=None, alias="dqRag")
+    bods_compliance: Optional[bool] = Field(default=None, alias="bodsCompliance")
 
-    def dict(self, *args, **kwargs):
+    def model_dump_json(self, *args, **kwargs):
         """Custom dict method to convert list of noc strings to a cvs.
 
         There's a bug in the BODS timetables API where ?noc=NOC1&noc=NOC2
         only recognises the last parameter. This also applies to adminArea too.
         """
-        dict_ = super().dict(*args, **kwargs)
+        dict_ = super().model_dump(*args, **kwargs)
 
         if "noc" in dict_:
             dict_["noc"] = ",".join(dict_.get("noc", []))
         if "adminArea" in dict_:
             dict_["adminArea"] = ",".join(dict_.get("adminArea", []))
-        return dict_
+        return json.dumps(dict_)
 
 
 class TimetableResponse(BaseAPIResponse):
